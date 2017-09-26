90952e986e52b0d87d4dbd2b4c4e48d0301732d3
Matt Warman <mkwarman@gmail.com>
2017-09-25 20:39:37 -0400

    broke api logic out to seperate module

---
+++ api.py
@@ -0,0 +1,58 @@
+import os
+from slackclient import SlackClient
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def get_user_id(user_name):
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == user_name:
+                user_id = user.get('id')
+                print("Got target user id for '" + user['name'] +"': " + user_id)
+                return user_id
+    else:
+        print("could not find bot user with the name " + user_name)
+
+def get_user_name(user_id):
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        users = api_call.get('members')
+        for user in users:
+            if 'id' in user and user.get('id') == user_id:
+                print("Got target user name for '" + user['id'] + "': " + user.get('name'))
+                return user.get('name')
+
+def get_channel_name(channel_id):
+    print("channel_id: " + channel_id)
+    api_call = slack_client.api_call("channels.info", channel=channel_id)
+    print(api_call)
+    if api_call.get('ok'):
+        channel = api_call.get('channel')
+        print(channel)
+        if 'id' in channel and channel.get('id') == channel_id:
+            print("Got target channel name for '" + channel['id'] + "': " + channel.get('name'))
+            return channel.get('name')
+
+def get_group_name(group_id):
+    print("group_id: " + group_id)
+    api_call = slack_client.api_call("groups.info", channel=group_id)
+    print(api_call)
+    if api_call.get('ok'):
+        group = api_call.get('group')
+        print(group)
+        if 'id' in group and group.get('id') == group_id:
+            print("Got target group name for '" + group['id'] + "': " + group.get('name'))
+            return group.get('name')
+
+def get_name_from_id(id):
+    if id.startswith("U"):
+        return get_user_name(id)
+    elif id.startswith("C"):
+        return get_channel_name(id)
+    elif id.startswith("G"):
+        return get_group_name(id)
+    else:
+        print("get_name_from_id encountered unhandled ID")
--- stokebot.py
+++ stokebot.py
@@ -2,6 +2,7 @@ import os
 import time
 import definition_model
 import dao
+import api
 from slackclient import SlackClient
 
 # constants
@@ -16,61 +17,6 @@ global at_target_user_id
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-
-
-def get_user_id(user_name):
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == user_name:
-                user_id = user.get('id')
-                print("Got target user id for '" + user['name'] +"': " + user_id)
-                return user_id
-    else:
-        print("could not find bot user with the name " + BOT_NAME)
-
-def get_user_name(user_id):
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        users = api_call.get('members')
-        for user in users:
-            if 'id' in user and user.get('id') == user_id:
-                print("Got target user name for '" + user['id'] + "': " + user.get('name'))
-                return user.get('name')
-
-def get_channel_name(channel_id):
-    print("channel_id: " + channel_id)
-    api_call = slack_client.api_call("channels.info", channel=channel_id)
-    print(api_call)
-    if api_call.get('ok'):
-        channel = api_call.get('channel')
-        print(channel)
-        if 'id' in channel and channel.get('id') == channel_id:
-            print("Got target channel name for '" + channel['id'] + "': " + channel.get('name'))
-            return channel.get('name')
-
-def get_group_name(group_id):
-    print("group_id: " + group_id)
-    api_call = slack_client.api_call("groups.info", channel=group_id)
-    print(api_call)
-    if api_call.get('ok'):
-        group = api_call.get('group')
-        print(group)
-        if 'id' in group and group.get('id') == group_id:
-            print("Got target group name for '" + group['id'] + "': " + group.get('name'))
-            return group.get('name')
-
-def get_name_from_id(id):
-    if id.startswith("U"):
-        return get_user_name(id)
-    elif id.startswith("C"):
-        return get_channel_name(id)
-    elif id.startswith("G"):
-        return get_group_name(id)
-    else:
-        print("get_name_from_id encountered unhandled ID")
 
 def handle_target_user_text(text, channel, message_data):
     print("Handling target user text")
@@ -113,10 +59,10 @@ def handle_add_definition(text, channel, message_data):
     print("definition: " + definition)
 
     definition_object = definition_model.Definition()
-    user_name = get_user_name(message_data['user'])
+    user_name = api.get_user_name(message_data['user'])
     print("user_name: " + user_name)
     print(message_data)
-    channel_name = get_name_from_id(message_data['channel'])
+    channel_name = api.get_name_from_id(message_data['channel'])
     print("channel_name: " + channel_name)
 
     definition_object.new(word, definition, user_name, channel_name)
@@ -128,14 +74,15 @@ if __name__ == "__main__":
         print("StokeBot connected and running!")
         global at_bot_id
         global at_target_user_id
-        at_bot_id = get_user_id(BOT_NAME) # Get the bot's ID
-        at_target_user_id = get_user_id(TARGET_USER_NAME) # Get the target user's ID
+        at_bot_id = api.get_user_id(BOT_NAME) # Get the bot's ID
+        at_target_user_id = api.get_user_id(TARGET_USER_NAME) # Get the target user's ID
 
         while True:
             text, channel, message_data = listen_for_user(slack_client.rtm_read())
             if text and channel:
                 # handle_command(command, channel)
                 handle_target_user_text(text, channel, message_data)
-                time.sleep(READ_WEBSOCKET_DELAY)
+            time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or Bot ID?")
+
