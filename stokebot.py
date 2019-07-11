a7a2a8989e7f9c51d4aeede612f22c3f14a95e4c
Matthew Warman <mwarman@paylocity.com>
2019-07-11 18:16:55 -0400

    more testing logic

--- stokebot.py
+++ stokebot.py
@@ -12,6 +12,24 @@ sys.path.append('core')
 # # Append feature modules directory
 # sys.path.append('features')
 
+@slack.RTMClient.run_on(event='message')
+def say_hello(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    print("data" + str(data))
+    trigger_text = ("Hello %s" % (os.getenv("BOT_NAME")))
+    if 'text' in data and trigger_text in data['text']:
+        channel_id = os.getenv("TEST_CHANNEL_ID")
+        thread_ts = data['ts']
+        text = "Hi, <@%s>!" % (data['user'])
+        
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=text,
+            thread_ts=thread_ts
+        )
+
 if __name__ == "__main__":
     client = slack.WebClient(token=os.getenv("SLACK_TOKEN"))
 
@@ -20,3 +38,6 @@ if __name__ == "__main__":
             text="Hello world!")
     assert response["ok"]
     assert response["message"]["text"] == "Hello world!"
+
+    rtm_client = slack.RTMClient(token=os.getenv("SLACK_TOKEN"))
+    rtm_client.start()
